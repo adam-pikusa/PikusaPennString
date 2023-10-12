3bb0b8f83f0d327e99b937bbcb8ca5f2eb2f0bd8
adam-pikusa <114311367+adam-pikusa@users.noreply.github.com>
2023-10-13 01:11:20 +0200

    now works on linux

--- include/technikumSTL/string.h
+++ include/technikumSTL/string.h
@@ -13,12 +13,12 @@ namespace technikum
         class Iterator
         {
         public:
-            using difference_type = std::ptrdiff_t;
-            using element_type = T;
-            using pointer = element_type*;
-            using reference = element_type&;
+            typedef std::random_access_iterator_tag iterator_category; 
+            typedef std::ptrdiff_t difference_type;
+            typedef T value_type;
+            typedef T* pointer;
+            typedef T& reference;
         public:
-            Iterator() {}
             Iterator(pointer ptr) : _ptr(ptr) {}
             auto& operator++() { ++_ptr; return *this; }
             auto& operator--() { --_ptr; return *this; }
@@ -32,12 +32,21 @@ namespace technikum
                 --_ptr;
                 return temp;
             }
+            auto& operator+=(difference_type value) { _ptr += value; return *this; }
+            auto& operator-=(difference_type value) { _ptr -= value; return *this; }
+            auto operator+(const Iterator& other) const { return _ptr + other._ptr; }
+            auto operator-(const Iterator& other) const { return _ptr - other._ptr; }
+            auto operator+(difference_type value) const { return _ptr + value; }
+            auto operator-(difference_type value) const { return _ptr - value; }
             reference operator*() { return *_ptr; }
+            reference operator[](difference_type n) const { return *(_ptr + n); }
             auto* operator->() { return _ptr; }
-            bool operator==(const Iterator& a) { return _ptr == a._ptr; }
-            bool operator!=(const Iterator& a) { return _ptr != a._ptr; }
+            bool operator==(const Iterator& other) const { return _ptr == other._ptr; }
+            bool operator!=(const Iterator& other) const { return _ptr != other._ptr; }
+            bool operator==(pointer other) const { return _ptr == other; }
+            bool operator!=(pointer other) const { return _ptr != other; }
         private:
-            T* _ptr;
+            pointer _ptr;
         };
 
         // can be increased if bigger sso buffer is desired
--- tests/iterator_test.cpp
+++ tests/iterator_test.cpp
@@ -15,3 +15,29 @@ TEST_CASE("iterators")
 
 	std::cout << (std::find(test.cbegin(), test.cend(), 'W') != test.cend()) << '\n';
 }
+
+TEST_CASE("subscript iterator")
+{
+	const technikum::string test("Hello World");
+	CHECK_EQ(test.cbegin()[4], 'o');
+}
+
+TEST_CASE("iterator and int addition")
+{
+	const technikum::string test("Hello World");
+	CHECK_EQ(&*test.cend(), test.cbegin() + 11); 
+}
+
+TEST_CASE("iterator and int subtraction")
+{
+	const technikum::string test("Hello World");
+	CHECK_EQ(&*test.cbegin(), test.cend() - 11); 
+}
+
+TEST_CASE("iterator add assign")
+{
+	const technikum::string test("Hello World");
+	auto it = test.cbegin();
+	it += 3;
+	CHECK_EQ(&*it, test.cbegin() + 3);
+}
