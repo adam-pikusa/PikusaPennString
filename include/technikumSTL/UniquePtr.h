f5e3ad33beeded02934ca71bb78debd3b911d58f
adam-pikusa <114311367+adam-pikusa@users.noreply.github.com>
2023-10-19 14:31:28 +0200

    added method declarations to uniqueptr

--- include/technikumSTL/UniquePtr.h
+++ include/technikumSTL/UniquePtr.h
@@ -3,6 +3,17 @@ namespace technikum
 	template<typename T>
 	class UniquePtr
 	{
+	public:
+		UniquePtr();
 
+		~UniquePtr();
+
+		T& operator*();
+		T* operator->();
+		operator bool() const;
+
+		T* Release();
+		void Reset();
+		void Swap(T* other);
 	};
 }
