643219429c17776e053852783e149f71f1bef264
adam-pikusa <114311367+adam-pikusa@users.noreply.github.com>
2023-10-19 14:37:44 +0200

    modified uniqueptr decl

--- include/technikumSTL/UniquePtr.h
+++ include/technikumSTL/UniquePtr.h
@@ -4,16 +4,19 @@ namespace technikum
 	class UniquePtr
 	{
 	public:
-		UniquePtr();
+		UniquePtr(T* pointer) : pointer(pointer);
 
 		~UniquePtr();
 
-		T& operator*();
-		T* operator->();
-		operator bool() const;
+		T& operator*() { return *_pointer; }
+		T* operator->() const { return _pointer; };
+		operator bool() const { return _pointer != nullptr; };
 
 		T* Release();
 		void Reset();
 		void Swap(T* other);
+
+	private:
+		T* _pointer;
 	};
 }
